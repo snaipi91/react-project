01f463926d78f3c0a121263dc09888dd1d427930
snaipi91 <snaipi91@rambler.ru>
2016-10-25 16:24:39 +0300

    Version 0.1 Shop map

--- src/components/shop/shopItems.js
+++ src/components/shop/shopItems.js
@@ -2,20 +2,20 @@
 
 import React from 'react';
 import {Link} from 'react-router';
-import SkyLight from 'react-skylight';
 
 class ShopItems extends React.Component{
 
     render() {
-        console.log(this);
+
         if(this.props.data.items.length > 1) {
 
+            var popup = this.props.popup;
             var shopTemplate = this.props.data.items.map(function(item, index) {
 
                 return (
                     <Link to={`/shop/${index}`} key={index}>
-                        <div onClick={() => this.refs.simpleDialog.show()} className={"col-lg-4 " + "col-md-6 " + "col-sm-6 " + "col-xs-12"}>
-                            <div className="object__component" data-geo={item.Cells.geoData.coordinates[0] + ',' + item.Cells.geoData.coordinates[1]}>
+                        <div onClick={() => popup.simpleDialog.show()} className={"col-lg-4 " + "col-md-6 " + "col-sm-6 " + "col-xs-12"}>
+                            <div className="object__component">
                                 <p className="object__name"><span>Название магазина: </span>{item.Cells.Name}</p>
                                 <p className="object__address"><span>Адрес: </span>{item.Cells.Address}</p>
                                 <p className="object__district"><span>Округ: </span>{item.Cells.AdmArea}</p>
@@ -33,9 +33,6 @@ class ShopItems extends React.Component{
         return (
             <div className="shops">
                 {shopTemplate}
-                <SkyLight hideOnOverlayClicked ref="simpleDialog" title="Hi, I'm a simple modal">
-                    Hello, I dont have any callback.
-                </SkyLight>
             </div>
         );
 
--- src/components/shop/shopPage.js
+++ src/components/shop/shopPage.js
@@ -4,41 +4,72 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import ShopItems from './shopItems';
+import SkyLight from 'react-skylight';
+import { Map, Marker, MarkerLayout } from 'yandex-map-react';
 
 import * as shopAction from '../../actions/shopActions';
 
 class Shop extends React.Component {
     constructor(props, context) {
-        console.log('Конструктор');
         super(props, context);
         this.onLoadingItems = this.onLoadingItems.bind(this);
+        this.onLoadingMap = this.onLoadingMap.bind(this);
     }
 
     componentWillMount() {
-        console.log('До рендера');
         this.props.shopAction.loadItem(3);
     }
 
     componentDidMount() {
-        console.log('После рендера');
     }
 
     componentWillReceiveProps() {
-        console.log('Изменили props');
     }
 
     onLoadingItems() {
         this.props.shopAction.loadItem(this.props.shop.loadItems);
     }
 
+    onLoadingMap(id) {
+        let _points = [];
+
+        if(this.props.shop.items.length > 1) {
+
+            _points[0] = this.props.shop.items[id].Cells.geoData.coordinates[0];
+            _points[1] = this.props.shop.items[id].Cells.geoData.coordinates[1];
+
+        }
+
+        return _points;
+    }
+
     render() {
+
+        var _id = (this.props.params.id == undefined) ? 0 : this.props.params.id;
         var _stateRequest;
+        var _myBigGreenDialog = {
+            backgroundColor: 'rgb(27, 25, 25)',
+            color: '#ffffff',
+            width: '50%',
+            padding: '15px',
+            marginLeft: '-25%'
+        };
+
+        console.log(this.onLoadingMap(_id)[0]);
+
         (this.props.shop.request == true) ? _stateRequest = 'request' : _stateRequest = 'no-request';
 
         return (
             <div className={"app " + "container-fluid"}>
                 <h1>Shops</h1>
-                <ShopItems data={this.props.shop} />
+                <SkyLight dialogStyles={_myBigGreenDialog} hideOnOverlayClicked ref="simpleDialog">
+                    <div className="popup__title">{(this.props.shop.items.length > 1) ? this.props.shop.items[_id].Cells.Name : 'Title'}</div>
+                    <div className="popup__address">{(this.props.shop.items.length > 1) ? this.props.shop.items[_id].Cells.Address : 'address'}</div>
+                    <Map className="popup__map" width="920px" height="300px" center={[this.onLoadingMap(_id)[1], this.onLoadingMap(_id)[0]]} zoom={15}>
+                        <Marker lat={this.onLoadingMap(_id)[1]} lon={this.onLoadingMap(_id)[0]} />
+                    </Map>
+                </SkyLight>
+                <ShopItems popup={this.refs} data={this.props.shop} />
                 <p className={"bg-success"}>Всего загруженно объектов - {this.props.shop.items.length}</p>
                 <div className={"bg-danger"}>{this.props.shop.itemsError}</div>
                 <div>
@@ -51,7 +82,6 @@ class Shop extends React.Component {
 }
 
 const mapStateToProps = (state) => {
-    console.log('Connect');
     return {
         shop: state.shop
     };
